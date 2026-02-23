45f3e92250f0f549bd86f704eeb1bbc503332a36
Syed Hammad Ali <syedhammad.cs@gmail.com>
2026-02-23 23:36:03 +0530

    Restrict TaskViewSet to authenticated user's projects

--- api/views.py
+++ api/views.py
@@ -17,5 +17,11 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(project__user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save()
